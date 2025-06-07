5a05406f5c55539b56b4c91d60b7d84aad99f0ed
Nick Allen <nick.allen@thinkfresh.digital>
2025-06-07 15:40:26 +0100

    component scaffolding template

--- sitecore.cli.config.ts
+++ sitecore.cli.config.ts
@@ -11,4 +11,38 @@ export default defineCliConfig({
       }),
     ],
   },
+  scaffold: {
+    templates: [
+      {
+        name: "fixDefault",
+        generateTemplate: (componentName: string) => {
+          return `import React, { JSX } from 'react';
+import { ComponentParams, ComponentRendering } from '@sitecore-content-sdk/nextjs';
+
+interface ${componentName}Props {
+  rendering: ComponentRendering & { params: ComponentParams };
+  params: ComponentParams;
+}
+
+export const Default = (props: ${componentName}Props): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div className={\`component \${props.params.styles}\`} id={id ? id : undefined}>
+      <div className="component-content">
+        <p>${componentName} Component</p>
+      </div>
+    </div>
+  );
+};`;
+        },
+        getNextSteps: (componentOutputPath: string) => {
+          return [
+            `component path is here: ${componentOutputPath}`,
+          ];
+        },
+        fileExtension: 'tsx',
+      },
+    ],
+  },
 });
